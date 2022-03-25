7e57082b399c83cdb3196cc114c63b8d76831b4f
ISMa1814 <98503060+ISMa1814@users.noreply.github.com>
2022-03-25 15:51:38 -0600

    Falta el FrmPedido

--- Examen_II_Juan_Hernandez/Examen_II_Juan_Hernandez/FrmMenu.cs
+++ Examen_II_Juan_Hernandez/Examen_II_Juan_Hernandez/FrmMenu.cs
@@ -16,6 +16,7 @@ namespace Examen_II_Juan_Hernandez
         }
 
         FrmProducto frmProducto = null;
+        FrmPedido frmPedido = null;
 
         private void ProductosToolStripButton_Click(object sender, EventArgs e)
         {
@@ -37,10 +38,24 @@ namespace Examen_II_Juan_Hernandez
             frmProducto = null;
         }
 
-        //---------------------------------------------------------------
         private void PedidosToolStripButton_Click(object sender, EventArgs e)
         {
+            if (frmPedido == null)
+            {
+                frmPedido = new FrmPedido();
+                frmPedido.MdiParent = this;
+                frmPedido.FormClosed += FrmPedido_FormClosed; ;
+                frmPedido.Show();
+            }
+            else
+            {
+                frmPedido.Activate();
+            }
+        }
 
+        private void FrmPedido_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            frmPedido = null;
         }
     }
 }
---
+++ Examen_II_Juan_Hernandez/Examen_II_Juan_Hernandez/FrmPedido.cs
@@ -0,0 +1,36 @@
+﻿using Datos.Entidades;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Examen_II_Juan_Hernandez
+{
+    public partial class FrmPedido : Form
+    {
+        public FrmPedido()
+        {
+            InitializeComponent();
+        }
+
+        Pedido pedido = new Pedido();
+        Producto Producto;
+
+        //Lista para almacenar los detalles del pedido
+        List<DetallePedido> detallePedidosLista = new List<DetallePedido>();
+
+        decimal subTotal = 0;
+        decimal isv = 0;
+        decimal totalAPagar = 0;
+
+        private void FrmPedido_Load(object sender, EventArgs e)
+        {
+            DetalleDataGridView.DataSource = detallePedidosLista;
+        }
+    }
+}
